815eb0aec9a5f23cb37b5bd834a300e28d3e0d03
Krommak <60076342+Krommak@users.noreply.github.com>
2023-02-23 00:55:46 +0300

    Добавьте файлы проекта.

---
+++ Assets/Source/Scripts/FlowField/FieldInit/FieldInitSystem.cs
@@ -0,0 +1,12 @@
+using Leopotam.Ecs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldInitSystem : IEcsInitSystem
+{
+    public void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+}
